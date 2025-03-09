175ee470cb98d735e14f39b52a112715308b71e1
Warshanu Chandrabadara <waraconcepts@gmail.com>
2025-03-09 22:26:50 +0530

    Removed fortify and changed the home page
    Nuski please fix the css of the homepage

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
 
