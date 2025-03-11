9ab3635cced8ac6f1404f29e171a58e4842ca350
Warshanu Chandrabadara <waraconcepts@gmail.com>
2025-03-11 22:19:11 +0530

    Update resource and class migration

--- database/migrations/2025_03_11_111153_resource_and_classes.php
+++ database/migrations/2025_03_11_111153_resource_and_classes.php
@@ -24,6 +24,52 @@ return new class extends Migration
             $table->string('course');
             $table->timestamps();
         });
+
+        Schema::create('class_rooms', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('department_id');
+            $table->int('capacity')
+            $table->timestamps();
+        });
+
+        Schema::create('resources', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('department_id');
+            $table->string('name');
+            $table->int('qty');
+            $table->timestamps();
+        });
+
+        Schema::create('resource_reserve', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('resource_id');
+            $table->foreignId('user_id');
+            $table->dateTime('request_dateTime', precision: 0);
+            $table->dateTime('handover_dateTime', precision: 0);
+            $table->string('status');
+            $table->int('qty');
+            $table->timestamps();
+        });
+
+        Schema::create('class_reserve', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('class_id');
+            $table->foreignId('user_id');
+            $table->dateTime('request_dateTime', precision: 0);
+            $table->dateTime('handover_dateTime', precision: 0);
+            $table->string('status');
+            $table->timestamps();
+        });
+
+        Schema::create('events', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->foreignId('user_id');
+            $table->dateTime('request_dateTime', precision: 0);
+            $table->dateTime('handover_dateTime', precision: 0);
+            $table->string('status');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -32,5 +78,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('department');
+        Schema::dropIfExists('courses');
+        Schema::dropIfExists('class_rooms');
+        Schema::dropIfExists('resources');
+        Schema::dropIfExists('resource_reserve');
+        Schema::dropIfExists('class_reserve');
+        Schema::dropIfExists('events');
     }
 };
