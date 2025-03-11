9c39b9496900c17d631b9e173763950099b83cc8
Nuski Nawfer <nuskinawfer@pm.me>
2025-03-11 23:48:28 +0530

    Fixed a bug in the "Registration" module where register form doesn't pass "last_name" and column "usertype" expects default value

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('frist_name');
-            $table->string('last_name');
+            $table->string('name');
+            // $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('usertype');
+            $table->string('usertype')->default('non-admin');
             $table->rememberToken();
             $table->timestamps();
             $table->integer('two_factor_code')->nullable();
