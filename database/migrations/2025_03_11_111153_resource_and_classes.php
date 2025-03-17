db7d35eb83bf369ba82af3557b741edfb6e9ac62
Nuski Nawfer <nuskinawfer@pm.me>
2025-03-18 05:15:21 +0530

    DB Structure Bug Fix

--- database/migrations/2025_03_11_111153_resource_and_classes.php
+++ database/migrations/2025_03_11_111153_resource_and_classes.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,12 +17,12 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('courses', function (Blueprint $table) {
-            $table->id()->primary();
-            $table->foreignId('department_id');
-            $table->string('course');
-            $table->timestamps();
-        });
+        // Schema::create('courses', function (Blueprint $table) {
+        //     $table->id()->primary();
+        //     $table->foreignId('department_id');
+        //     $table->string('course');
+        //     $table->timestamps();
+        // });
 
         Schema::create('class_rooms', function (Blueprint $table) {
             $table->id()->primary();
@@ -78,7 +77,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('departments');
-        Schema::dropIfExists('courses');
+        // Schema::dropIfExists('courses');
         Schema::dropIfExists('class_rooms');
         Schema::dropIfExists('resources');
         Schema::dropIfExists('resource_reserve');
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ResourcesController;
+use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TwoFactorController;
-use App\Http\Controllers\ResourceController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\TimetableController;
-use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\student\studentController;
 use App\Http\Controllers\lecturer\lecturerController;
 use App\Http\Controllers\admin\adminController;
@@ -20,7 +16,6 @@ use App\Http\Controllers\AnnouncementController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
@@ -35,8 +30,8 @@ Route::middleware('auth', 'twofactor')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/resources',[ResourceController::class,'index'])->name('resources');
-    Route::post('/resources', [ResourceController::class,'store'])->name('resources.store');
+    Route::get('/resources', [ResourceController::class, 'index'])->name('resources');
+    Route::post('/resources', [ResourceController::class, 'store'])->name('resources.store');
     Route::get('/announcements', [AnnouncementController::class, 'index'])->name('announcements');
     Route::resource('courses', CourseController::class);
     Route::get('/courses/create', [CourseController::class, 'create'])->name('courses.create');
@@ -46,20 +41,19 @@ Route::middleware('auth', 'twofactor')->group(function () {
 // Academics Section
 Route::get('/courses', [CourseController::class, 'index'])->name('courses')->middleware('auth');
 Route::get('/timetable', [TimetableController::class, 'index'])->name('timetable')->middleware('auth');
-    Route::get('/announcements', [AnnouncementsController::class, 'index'])->name('announcements');
-    Route::get('/courses/create', [CourseController::class, 'create'])->name('courses.create');
-    Route::resource('courses', CourseController::class);
-});
+Route::get('/announcements', [AnnouncementController::class, 'index'])->name('announcements');
+Route::get('/courses/create', [CourseController::class, 'create'])->name('courses.create');
+Route::resource('courses', CourseController::class);
 
-Route::middleware('auth','twofactor', 'studentMiddleware')->group(function () {
+Route::middleware('auth', 'twofactor', 'studentMiddleware')->group(function () {
     Route::get('/dashboard', [studentController::class, 'index'])->name('dashboard');
 });
 
-Route::middleware('auth','twofactor', 'lecturerMiddleware')->group(function () {
+Route::middleware('auth', 'twofactor', 'lecturerMiddleware')->group(function () {
     Route::get('/lecturer/dashboard', [lecturerController::class, 'index'])->name('lecturer.dashboard');
 });
 
-Route::middleware('auth','twofactor', 'adminMiddleware')->group(function () {
+Route::middleware('auth', 'twofactor', 'adminMiddleware')->group(function () {
     Route::get('/admin/dashboard', [adminController::class, 'index'])->name('admin.dashboard');
     Route::get('/admin/register-student', [StudentRegisterController::class, 'showStudentRegistrationForm'])->name('register.student.form');
     Route::post('/register-student', [StudentRegisterController::class, 'registerStudent'])->name('register.student');
@@ -70,6 +64,6 @@ Route::get('/courses', [CourseController::class, 'index'])->name('courses')->mid
 Route::get('/timetable', [TimetableController::class, 'index'])->name('timetable')->middleware('auth');
 Route::get('/results', [ResultsController::class, 'index'])->name('results')->middleware('auth');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 
