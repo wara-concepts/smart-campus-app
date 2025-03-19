05d499b92031eba4b6612dd5526a0e3fd21aa6d5
Nuski Nawfer <nuskinawfer@pm.me>
2025-03-19 14:47:51 +0530

    Backup of auth.php and Merged all routes to web.php to avoid ERR_TOO_MANY_REDIRECTS

--- routes/auth_backup.php
+++ routes/auth_backup.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ResourceReservationController;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
