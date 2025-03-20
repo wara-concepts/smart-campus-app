654fcedbed140138a4670eb6491b156662a61818
Nuski Nawfer <nuskinawfer@pm.me>
2025-03-20 19:44:24 +0530

    Refactor timetable migration and restore courses table migration

--- database/migrations/2025_03_15_121029_create_timetables_table.php
+++ database/migrations/2025_03_15_121029_create_timetables_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('timetables', function (Blueprint $table) {
             $table->id(); // Auto-increment primary key
-            $table->unsignedBigInteger('course_id'); // Foreign key to courses table
+            $table->bigInteger('course_id'); // Foreign key to courses table
             $table->string('day', 20); // Day of the schedule (e.g., Monday)
             $table->time('start_time'); // Start time of the class
             $table->time('end_time'); // End time of the class
@@ -21,7 +21,7 @@ return new class extends Migration {
             $table->timestamps(); // created_at & updated_at
 
             // Foreign key constraint
-            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            //$table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
         });
     }
 
